761e47d70489c3e509a11bf455ea24770dd6e963
zachbil5 <55146215+zachbil5@users.noreply.github.com>
2020-09-03 22:41:23 -0400

    Day 1
    Added PlayerController and ability to roll ball aaround

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0;
+    private Rigidbody rigB;
+    private float moveX, moveY;
+
+    void Start()
+    {
+        rigB = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue inputVal)
+    {
+        Vector2 movementVec = inputVal.Get<Vector2>();
+        moveX = movementVec.x;
+        moveY = movementVec.y;
+    }
+
+    void FixedUpdate()
+    {
+        rigB.AddForce(new Vector3(moveX, 0.0f, moveY) * speed);
+    }
+}
