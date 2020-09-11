af50e923994e16eb8a63d2b94d75f285bd32da7c
zachbil5 <55146215+zachbil5@users.noreply.github.com>
2020-09-10 21:42:50 -0400

    AAdded pickups and walls and score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 0;
+    public TextMeshProUGUI countText;
     private Rigidbody rigB;
     private float moveX, moveY;
+    private int count;
+
 
     void Start()
     {
         rigB = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
     }
 
     void OnMove(InputValue inputVal)
@@ -25,5 +32,20 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         rigB.AddForce(new Vector3(moveX, 0.0f, moveY) * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text += count.ToString();
     }
 }
